1e355260ed0120e2fdfafd7969cc4c8c7f21ab5c
elcin2008-ajin <elcinmahmudov123elcinn@gmail.com>
2025-12-01 14:54:30 +0000

    JJ

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
