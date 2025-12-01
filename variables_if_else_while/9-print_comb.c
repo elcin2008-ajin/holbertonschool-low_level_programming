7b342abb30256a14fd7cf2e3272bb5613cb7e1b5
elcin2008-ajin <elcinmahmudov123elcinn@gmail.com>
2025-12-01 14:57:08 +0000

    jj

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers separated by comma and space
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
