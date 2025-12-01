8343ca6d5c7c8b4197263ac866d3c3a058cd2b44
elcin2008-ajin <elcinmahmudov123elcinn@gmail.com>
2025-12-01 14:28:45 +0000

    jj

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
+/**
+ * main - prints lowercase then uppercase alphabet
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    char lower = 'a';
-    char upper = 'A';
+	char lower = 'a';
+	char upper = 'A';
 
-    while (lower <= 'z')
-    {
-        putchar(lower);
-        lower++;
-    }
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
 
-    while (upper <= 'Z')
-    {
-        putchar(upper);
-        upper++;
-    }
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
