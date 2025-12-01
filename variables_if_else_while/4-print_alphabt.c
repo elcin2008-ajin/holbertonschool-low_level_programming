2c4c154a1c98e9341653c4ccb1a5e92879c4f90a
elcin2008-ajin <elcinmahmudov123elcinn@gmail.com>
2025-12-01 14:46:24 +0000

    jj

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
