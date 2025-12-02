4490c19112b4f35687eef75ab71316f714e6d18f
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-02 07:07:15 +0000

    jj

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,29 +2,34 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
  */
 void times_table(void)
 {
-    int i, j, prod;
+	int i, j, prod, tens, ones;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            prod = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			tens = prod / 10;
+			ones = prod % 10;
 
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (prod < 10)
-                    _putchar(' ');
-            }
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
-            if (prod >= 10)
-                _putchar(prod / 10 + '0');
-            _putchar(prod % 10 + '0');
-        }
-        _putchar('\n');
-    }
+			if (prod >= 10)
+				_putchar(tens + '0');
+			else if (j != 0)
+				_putchar(' ');
+
+			_putchar(ones + '0');
+		}
+		_putchar('\n');
+	}
 }
