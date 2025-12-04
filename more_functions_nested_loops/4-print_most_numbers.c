3db23d28cb84a68bb574274b4df2aa548b009175
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-04 05:34:03 +0000

    Jj

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - 0-dan 9-a qədər ədədləri çap edir
+ * print_most_numbers - 0-dan 9-a qədər ədədləri çap edir,
  * 2 və 4-ü çıxmaq şərti ilə, sonra yeni sətr əlavə olunur.
  */
 void print_most_numbers(void)
@@ -10,8 +10,8 @@ void print_most_numbers(void)
 
     for (i = 0; i <= 9; i++)
     {
-        if (i != 2 && i != 4)   // 2 və 4-ü atırıq
-            _putchar(i + '0');  // ədədin simvolunu çap edirik
+        if (i != 2 && i != 4)   /* 2 və 4-ü atırıq */
+            _putchar(i + '0');  /* ədədin simvolunu çap edirik */
     }
-    _putchar('\n');            // yeni sətr
+    _putchar('\n');            /* yeni sətr */
 }
