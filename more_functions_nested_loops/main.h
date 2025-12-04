0825e6589f0c9b88943e892122ef7facc195b3db
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-04 05:42:08 +0000

    kk

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,8 +4,9 @@
 /* _putchar prototype */
 int _putchar(char c);
 
-/* Function prototypes for your exercises */
+/* Function prototypes for the exercises */
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif /* MAIN_H */
