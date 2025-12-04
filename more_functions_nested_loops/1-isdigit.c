010c9e3e9e0149e8195174581fe52354e70f5382
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-04 05:17:14 +0000

    hh

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,13 @@
 
 /**
  * _isdigit - checks if a character is a digit
- * @c: the character to check
+ * @c: character to check
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-        return (1);
-    else
-        return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
