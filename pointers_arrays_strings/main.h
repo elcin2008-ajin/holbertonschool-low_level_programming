f796619f3d10415305b4fb2f5e02cb88fca43550
elcin2008-ajin <elcinmahmudov123elcin>
2025-12-08 06:44:47 +0000

    jj

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,33 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototypes for functions from 0 to 7 exercises */
-
-/* 0. reset_to_98.c */
+/* 0. Takes a pointer to an int and updates the value it points to 98 */
 void reset_to_98(int *n);
 
-/* 1. swap_int.c */
+/* 1. Swaps the values of two integers */
 void swap_int(int *a, int *b);
 
-/* 2. _strlen.c */
+/* 2. Returns the length of a string */
 int _strlen(char *s);
 
-/* 3. _puts.c */
+/* 3. Prints a string, followed by a new line */
 void _puts(char *str);
 
-/* 4. print_rev.c */
+/* 4. Prints a string in reverse, followed by a new line */
 void print_rev(char *s);
 
-/* 5. rev_string.c */
+/* 5. Reverses a string */
 void rev_string(char *s);
 
-/* 6. puts2.c */
+/* 6. Prints every other character of a string, starting with the first character */
 void puts2(char *str);
 
-/* 7. puts_half.c */
+/* 7. Prints half of a string */
 void puts_half(char *str);
 
-/* Provided _putchar function */
+/* 8. Prints n elements of an array of integers */
+void print_array(int *a, int n);
+
+/* Provided _putchar function declaration */
 int _putchar(char c);
 
 #endif /* MAIN_H */
