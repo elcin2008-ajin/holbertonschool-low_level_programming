8bca85c8774cf2d627ee40fa25acec9cc693fe04
elcin2008-ajin <elcinmahmudov123elcin>
2025-12-08 07:31:33 +0000

    kk

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day from 00:00 to 23:59
+ *
+ * Return: void
  */
 void jack_bauer(void)
 {
-    int h, m;
+	int hour, minute;
 
-    for (h = 0; h < 24; h++)
-    {
-        for (m = 0; m < 60; m++)
-        {
-            _putchar(h / 10 + '0');
-            _putchar(h % 10 + '0');
-            _putchar(':');
-            _putchar(m / 10 + '0');
-            _putchar(m % 10 + '0');
-            _putchar('\n');
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
