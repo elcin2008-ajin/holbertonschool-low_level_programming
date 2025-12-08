b8193d7f556e224673e2a37f993200455bd5dcb2
elcin2008-ajin <elcinmahmudov123elcin>
2025-12-08 07:19:40 +0000

    jj

--- pointers_arrays_strings/7-print_last_digit.c
+++ pointers_arrays_strings/7-print_last_digit.c
@@ -1,14 +1,21 @@
 #include "main.h"
-
+/**
+* print_last_digit -function the absolute value of an integer
+*
+*@n: - last digit of n
+* Return: 1 for letters. Returns 0 otherwise
+*/
 int print_last_digit(int n)
 {
-    int last;
-
-    if (n < 0)
-        n = -n;
-
-    last = n % 10;
-    _putchar('0' + last);
-
-    return (last);
+int digit = n % 10;
+if (digit < 0)
+{
+_putchar(-digit + 48);
+return (-digit);
+}
+else
+{
+_putchar(digit + 48);
+}
+return (digit);
 }
