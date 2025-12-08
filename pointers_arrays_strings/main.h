6aaed70d239f390add59a000ff266fa55abe55bc
elcin2008-ajin <elcinmahmudov123elcin>
2025-12-08 07:00:30 +0000

    kk

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototypes */
 int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int print_last_digit(int n);
 
 #endif /* MAIN_H */
