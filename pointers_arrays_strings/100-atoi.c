b379a22deb341d7d709df0aab4bd0b0e0dbb329f
elcin2008-ajin <elcinmahmudov123elcin>
2025-12-08 06:52:21 +0000

    kk

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,16 +10,27 @@ int _atoi(char *s)
 {
 	int i = 0, sign = 1, num = 0;
 
+	/* Determine the sign */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
+		else if (s[i] == '+')
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			num = num * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
+			break;
+		i++;
+	}
+
+	/* Convert digits */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (sign == 1 && num > (2147483647 - (s[i] - '0')) / 10)
+			return (2147483647);
+		if (sign == -1 && num > (2147483648 - (s[i] - '0')) / 10)
+			return (-2147483648);
+
+		num = num * 10 + (s[i] - '0');
 		i++;
 	}
 
