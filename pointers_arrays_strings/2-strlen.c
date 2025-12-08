d99b04feb5b13e12e592ded462a7a9d057da475a
elcin2008-ajin <elcinmahmudov123elcin>
2025-12-08 06:22:28 +0000

    kk

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
-/**
- * _strlen - stringin uzunluğunu qaytarır
- * @s: ölçüləcək string
- *
- * Return: stringin uzunluğu
- */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-        len++;
-
-    return (len);
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
