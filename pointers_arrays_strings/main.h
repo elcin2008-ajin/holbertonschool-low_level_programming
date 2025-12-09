888c41e0ee31f53926554ceb6b89314cf62940c3
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-09 06:05:29 +0000

    jj

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,6 +15,9 @@ char *_strncpy(char *dest, char *src, int n);
 /* Task 3: compares two strings */
 int _strcmp(char *s1, char *s2);
 
+/* Task 4: reverses the content of an array of integers */
+void reverse_array(int *a, int n);
+
 /* _putchar prototype */
 int _putchar(char c);
 
