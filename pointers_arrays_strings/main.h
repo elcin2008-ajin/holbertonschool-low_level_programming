9879682d36bdf69bd3f5436e6ac56deac1ca65d4
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-09 05:59:44 +0000

    jj

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,14 +3,17 @@
 
 /* Function prototypes */
 
-/* Task 0 */
+/* Task 0: concatenates two strings */
 char *_strcat(char *dest, char *src);
 
-/* Task 1 */
+/* Task 1: concatenates at most n characters of src to dest */
 char *_strncat(char *dest, char *src, int n);
 
-/* Task 2 */
+/* Task 2: copies a string */
 char *_strncpy(char *dest, char *src, int n);
+
+/* Task 3: compares two strings */
+int _strcmp(char *s1, char *s2);
 
 /* _putchar prototype */
 int _putchar(char c);
