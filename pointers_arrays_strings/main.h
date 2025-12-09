b82455ffccf5fc7cb2661f6b9eb124566e7ac356
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-09 05:56:25 +0000

    kk

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,8 +2,17 @@
 #define MAIN_H
 
 /* Function prototypes */
+
+/* Task 0 */
 char *_strcat(char *dest, char *src);
+
+/* Task 1 */
 char *_strncat(char *dest, char *src, int n);
+
+/* Task 2 */
+char *_strncpy(char *dest, char *src, int n);
+
+/* _putchar prototype */
 int _putchar(char c);
 
 #endif /* MAIN_H */
