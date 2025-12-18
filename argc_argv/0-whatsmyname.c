8edc5e9f60dc7aba38bc14936322637f5166852b
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-18 11:03:12 +0000

    kk

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - Prints the name of the program
- * @argc: Number of arguments passed to the program
- * @argv: Array of pointers to the arguments
- *
- * Description: Prints the exact name used to call the program,
- * including the path, followed by a new line. If the program is renamed,
- * it will print the new name without recompilation.
- *
- * Return: 0 on success
- */
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
 int main(int argc, char *argv[])
 {
-    (void)argc; /* Unused parameter */
-
-    printf("%s\n", argv[0]);
-
-    return (0);
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
 }
