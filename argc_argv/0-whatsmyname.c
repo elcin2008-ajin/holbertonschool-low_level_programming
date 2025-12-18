3e116f6d12dbee4125640fa9cfbf31231f98f49e
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2025-12-18 10:57:49 +0000

    jj

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Description: This program prints its own name, as called
+ * on the command line, followed by a new line.
+ * If renamed, it prints the new name without recompiling.
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-(void)argc; /* unused warning */
+    (void)argc;
 
-printf("%s\n", argv[0]);
+    printf("%s\n", argv[0]);
 
-return (0);
+    return (0);
 }
