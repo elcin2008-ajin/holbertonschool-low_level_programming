a3281f63590ef8f5df0d9ef2827896128e3a357d
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-05 11:29:30 +0000

    kk

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,25 +2,25 @@
 #include <string.h>
 
 /**
- * get_op_func - returns a pointer to the correct function
+ * get_op_func - selects the correct function for the operator
  * @s: operator string
  * Return: pointer to function, or NULL if not found
  */
-int (*get_op_func(char *s))(int, int)
+int	(*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    if (s)  /* yalnız 1 if istifadə olunur */
-        while (ops[i].op && strcmp(s, ops[i].op) != 0)
-            i++;
+	if (s)
+		while (ops[i].op && strcmp(s, ops[i].op) != 0)
+			i++;
 
-    return (ops[i].op ? ops[i].f : NULL);
+	return (ops[i].op ? ops[i].f : NULL);
 }
