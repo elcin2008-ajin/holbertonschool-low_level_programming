b0d5e613085f3acfbc7a30821071b72de48ca430
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-05 11:24:04 +0000

    kk

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,12 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>  /* size_t */
+#include <stddef.h>  /* size_t, NULL */
 
 /* 0-print_name.c */
 void print_name(char *name, void (*f)(char *));
 
 /* 1-array_iterator.c */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* 2-int_index.c */
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
