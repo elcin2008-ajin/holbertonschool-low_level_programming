90f2e46ba86980e84e0e64bab6ab6261b03d6b2e
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-05 10:47:49 +0000

    KK

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,23 +1,36 @@
-#ifndef DOG_H
-#define DOG_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
 /**
- * struct dog - information about a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner's name
+ * dog_t *new_dog - function that initialize a variable of type struct dog
+ * @name: character
+ * @age: float
+ * @owner: character
+ * Return: Always 0.
  */
-struct dog
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-/* typedef for struct dog */
-typedef struct dog dog_t;
-
-/* Prototype of new_dog function */
-dog_t *new_dog(char *name, float age, char *owner);
-
-#endif /* DOG_H */
+dog_t *new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+{
+return (NULL);
+}
+new_dog->name = malloc(strlen(name) + 1);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+strcpy(new_dog->name, name);
+new_dog->age = age;
+new_dog->owner = malloc(strlen(owner) + 1);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+strcpy(new_dog->owner, owner);
+return (new_dog);
+}
