ce8426fdd7900a9434e7fcf0c8f6f48281783f55
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-05 10:07:33 +0000

    kk

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - function to typedef struct dog
- * @name: character pointer
- * @age: float
- * @owner: character pointer
+ * struct dog - it haqqında məlumat saxlayan struktur
+ * @name: itin adı
+ * @age: itin yaşı
+ * @owner: itin sahibi
  */
 typedef struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 } dog_t;
+
+/* Funksiya prototipləri */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /* DOG_H */
