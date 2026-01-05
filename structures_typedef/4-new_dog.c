ae94bb23907dd55b27ae4731348bd56bb9a9e99c
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-05 10:46:07 +0000

    jj

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,44 +1,23 @@
-#include <stdlib.h>
-#include <string.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * new_dog - creates a new dog
+ * struct dog - information about a dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to new dog, or NULL if it fails
+ * @owner: owner's name
  */
-dog_t *new_dog(char *name, float age, char *owner)
+struct dog
 {
-	dog_t *d;
+	char *name;
+	float age;
+	char *owner;
+};
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
+/* typedef for struct dog */
+typedef struct dog dog_t;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+/* Prototype of new_dog function */
+dog_t *new_dog(char *name, float age, char *owner);
 
-	d->name = malloc(strlen(name) + 1);
-	if (d->name == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-	strcpy(d->name, name);
-
-	d->owner = malloc(strlen(owner) + 1);
-	if (d->owner == NULL)
-	{
-		free(d->name);
-		free(d);
-		return (NULL);
-	}
-	strcpy(d->owner, owner);
-
-	d->age = age;
-
-	return (d);
-}
+#endif /* DOG_H */
