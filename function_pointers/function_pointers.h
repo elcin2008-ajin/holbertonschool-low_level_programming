6e3c2f22c5046bc1201dedf198ea0c77dc35babe
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-05 11:21:55 +0000

    kk

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/* Prototype for print_name */
+#include <stddef.h>  /* size_t */
+
+/* 0-print_name.c */
 void print_name(char *name, void (*f)(char *));
+
+/* 1-array_iterator.c */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
