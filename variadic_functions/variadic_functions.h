d1af8bae91af9275450921b69f05239dd6309424
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-06 19:28:45 +0000

    jj

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,13 +3,16 @@
 
 #include <stdarg.h>
 
-/* 0. sum_them_all */
+/* 0. sum_them_all - returns the sum of all its parameters */
 int sum_them_all(const unsigned int n, ...);
 
-/* 1. print_numbers */
+/* 1. print_numbers - prints numbers, followed by a new line */
 void print_numbers(const char *separator, const unsigned int n, ...);
 
-/* 2. print_strings */
+/* 2. print_strings - prints strings, followed by a new line */
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/* 3. print_all - prints anything, followed by a new line */
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
