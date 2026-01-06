c84003c46afb9aa84035c26fb471ffcaf59c9a94
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-06 19:26:58 +0000

    kk

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,8 +3,13 @@
 
 #include <stdarg.h>
 
-/* Prototypes */
+/* 0. sum_them_all */
 int sum_them_all(const unsigned int n, ...);
+
+/* 1. print_numbers */
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* 2. print_strings */
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
