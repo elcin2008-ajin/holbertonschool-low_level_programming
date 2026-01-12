520774e508a4c6b7a79fb83f1904a7f17e9053eb
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-12 12:05:02 +0000

    kk

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -9,33 +9,33 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
- * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
-/* Task 0: Print all elements of a doubly linked list */
+/* Task 0: Print list */
 size_t print_dlistint(const dlistint_t *h);
 
-/* Task 1: Return the number of elements in a doubly linked list */
+/* Task 1: List length */
 size_t dlistint_len(const dlistint_t *h);
 
-/* Task 2: Add a new node at the beginning of a doubly linked list */
+/* Task 2: Add node at the beginning */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
-/* Task 3: Add a new node at the end of a doubly linked list */
+/* Task 3: Add node at the end */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
-/* Task 4: Free a doubly linked list */
+/* Task 4: Free list */
 void free_dlistint(dlistint_t *head);
-
 
 /* Task 5: Get node at index */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* Task 6: Sum list */
+int sum_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
