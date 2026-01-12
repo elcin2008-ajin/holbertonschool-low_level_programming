3ed8c85f69b45ea478111b0379d92d9ce3dd0696
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-12 12:11:39 +0000

    kk

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,46 +2,43 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given index
- * @h: pointer to the head of the list
- * @idx: index where to insert the new node (starts at 0)
- * @n: data for the new node
+ * @h: pointer to the head
+ * @idx: index where to insert (starts at 0)
+ * @n: value of the new node
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: address of the new node, or NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current;
-    unsigned int i;
+	dlistint_t *new_node, *current;
+	unsigned int i;
 
-    if (!h)
-        return (NULL);
+	if (!h)
+		return (NULL);
 
-    /* Insert at the beginning */
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    current = *h;
-    for (i = 0; current && i < idx - 1; i++)
-        current = current->next;
+	current = *h;
+	for (i = 0; current && i < idx - 1; i++)
+		current = current->next;
 
-    if (!current)
-        return (NULL);
+	if (!current)
+		return (NULL);
 
-    /* If inserting at the end */
-    if (!current->next)
-        return (add_dnodeint_end(h, n));
+	if (!current->next)
+		return (add_dnodeint_end(h, n));
 
-    /* Insert in the middle */
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = current;
-    new_node->next = current->next;
+	new_node->n = n;
+	new_node->prev = current;
+	new_node->next = current->next;
 
-    current->next->prev = new_node;
-    current->next = new_node;
+	current->next->prev = new_node;
+	current->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
