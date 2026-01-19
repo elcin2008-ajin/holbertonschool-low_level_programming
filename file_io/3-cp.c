e6e0eb783e0755e462748eeb2235b07f36808e19
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-19 15:04:46 +0000

    kk

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,7 +6,7 @@
 /**
  * error_exit - Prints an error message and exits with code
  * @code: Exit code
- * @msg: Message to print
+ * @msg: Message to print (NULL if fd close)
  * @file: File name or descriptor
  */
 void error_exit(int code, char *msg, char *file)
@@ -15,7 +15,43 @@ void error_exit(int code, char *msg, char *file)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file);
 	else
 		dprintf(STDERR_FILENO, "%s %s\n", msg, file);
+
 	exit(code);
+}
+
+/**
+ * copy_buffer - Copies content from one fd to another
+ * @fd_from: Source file descriptor
+ * @fd_to: Destination file descriptor
+ */
+void copy_buffer(int fd_from, int fd_to)
+{
+	ssize_t r_bytes, w_bytes;
+	char buffer[1024];
+
+	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
+	{
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (w_bytes != r_bytes)
+			error_exit(99, "Error: Can't write to", "file_to");
+	}
+
+	if (r_bytes == -1)
+		error_exit(98, "Error: Can't read from file", "file_from");
+}
+
+/**
+ * close_fd - Closes a file descriptor and checks for error
+ * @fd: File descriptor
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		char fd_str[10];
+		sprintf(fd_str, "%d", fd);
+		error_exit(100, NULL, fd_str);
+	}
 }
 
 /**
@@ -28,8 +64,6 @@ void error_exit(int code, char *msg, char *file)
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
-	ssize_t r_bytes, w_bytes;
-	char buffer[1024];
 
 	if (ac != 3)
 	{
@@ -45,29 +79,9 @@ int main(int ac, char **av)
 	if (fd_to == -1)
 		error_exit(99, "Error: Can't write to", av[2]);
 
-	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
-	{
-		w_bytes = write(fd_to, buffer, r_bytes);
-		if (w_bytes != r_bytes)
-			error_exit(99, "Error: Can't write to", av[2]);
-	}
-
-	if (r_bytes == -1)
-		error_exit(98, "Error: Can't read from file", av[1]);
-
-	if (close(fd_from) == -1)
-	{
-		char fd_str[10];
-		sprintf(fd_str, "%d", fd_from);
-		error_exit(100, NULL, fd_str);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		char fd_str[10];
-		sprintf(fd_str, "%d", fd_to);
-		error_exit(100, NULL, fd_str);
-	}
+	copy_buffer(fd_from, fd_to);
+	close_fd(fd_from);
+	close_fd(fd_to);
 
 	return (0);
 }
