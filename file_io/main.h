dd00b9e806c2c41a6f86b24010611c4859d2f95b
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-19 14:52:42 +0000

    kk

---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Prototype of read_textfile function */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
