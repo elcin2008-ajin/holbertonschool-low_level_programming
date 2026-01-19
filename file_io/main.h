077734851d59d010bafd163ae5cec08180382645
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-19 14:58:23 +0000

    kk

--- file_io/main.h
+++ file_io/main.h
@@ -4,8 +4,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/* Prototypes */
+/* Prototypes for file I/O functions */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
