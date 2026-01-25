f16df79b05b5c1b65ee742f9073e2013bd3afcff
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-25 21:16:50 +0000

    jj

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -45,4 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 /* Task 4 */
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
+/* Task 5 */
+void hash_table_print(const hash_table_t *ht);
+
 #endif
