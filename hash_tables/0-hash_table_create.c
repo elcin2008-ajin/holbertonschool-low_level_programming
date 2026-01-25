a84c7c310293602de917eceb7fc0b356b09c5671
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-25 20:45:50 +0000

    kk

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the new hash table, or NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht;
+    unsigned long int i;
+
+    /* Əgər size 0-dırsa, mənasızdır */
+    if (size == 0)
+        return (NULL);
+
+    /* Hash table üçün memory ayır */
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
+
+    /* Array üçün memory ayır */
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL)
+    {
+        free(ht);
+        return (NULL);
+    }
+
+    /* Size-ı set et */
+    ht->size = size;
+
+    /* Array-ın bütün elementlərini NULL et */
+    for (i = 0; i < size; i++)
+        ht->array[i] = NULL;
+
+    return (ht);
+}
